e1f5a0a97dc7e0c1af206743b8ad190443903cc8
Will Ockelmann-Wagner <will@willwow.dev>
2023-08-16 00:16:45 -0700

    remove commented-out code about attributes for now

--- renderer/markdown/renderer.go
+++ renderer/markdown/renderer.go
@@ -1,8 +1,6 @@
 package markdown
 
 import (
-	"bytes"
-
 	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/renderer"
 	"github.com/yuin/goldmark/util"
@@ -23,7 +21,7 @@ func NewConfig() Config {
 // SetOption implements renderer.NodeRenderer.SetOption.
 func (c *Config) SetOption(name renderer.OptionName, value interface{}) {
 	if name == optTextWriter {
-		c.Writer = value.(Writer)
+		c.Writer, _ = value.(Writer)
 	}
 }
 
@@ -106,79 +104,30 @@ func (r *Renderer) writeLines(w util.BufWriter, source []byte, n ast.Node) {
 	l := n.Lines().Len()
 	for i := 0; i < l; i++ {
 		line := n.Lines().At(i)
-		r.Writer.RawWrite(w, line.Value(source))
+		r.Writer.Write(w, line.Value(source))
 	}
 }
-
-// GlobalAttributeFilter defines attribute names which any elements can have.
-var GlobalAttributeFilter = util.NewBytesFilter(
-	[]byte("accesskey"),
-	[]byte("autocapitalize"),
-	[]byte("autofocus"),
-	[]byte("class"),
-	[]byte("contenteditable"),
-	[]byte("dir"),
-	[]byte("draggable"),
-	[]byte("enterkeyhint"),
-	[]byte("hidden"),
-	[]byte("id"),
-	[]byte("inert"),
-	[]byte("inputmode"),
-	[]byte("is"),
-	[]byte("itemid"),
-	[]byte("itemprop"),
-	[]byte("itemref"),
-	[]byte("itemscope"),
-	[]byte("itemtype"),
-	[]byte("lang"),
-	[]byte("part"),
-	[]byte("role"),
-	[]byte("slot"),
-	[]byte("spellcheck"),
-	[]byte("style"),
-	[]byte("tabindex"),
-	[]byte("title"),
-	[]byte("translate"),
-)
 
 func (r *Renderer) renderDocument(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
 	// nothing to do
 	return ast.WalkContinue, nil
 }
 
-// HeadingAttributeFilter defines attribute names which heading elements can have
-var HeadingAttributeFilter = GlobalAttributeFilter
-
 func (r *Renderer) renderHeading(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	n := node.(*ast.Heading)
+	n, _ := node.(*ast.Heading)
 	if entering {
 		for i := 0; i < n.Level; i++ {
 			_, _ = w.WriteString("#")
 		}
 		_, _ = w.WriteString(" ")
-
-		// if n.Attributes() != nil {
-		// 	RenderAttributes(w, node, HeadingAttributeFilter)
-		// }
-		// _ = w.WriteByte('>')
 	} else {
 		_, _ = w.WriteString("\n\n")
 	}
 	return ast.WalkContinue, nil
 }
 
-// BlockquoteAttributeFilter defines attribute names which blockquote elements can have
-var BlockquoteAttributeFilter = GlobalAttributeFilter.Extend(
-	[]byte("cite"),
-)
-
 func (r *Renderer) renderBlockquote(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
 	if entering {
-		// if n.Attributes() != nil {
-		// 	_, _ = w.WriteString("```")
-		// 	// RenderAttributes(w, n, BlockquoteAttributeFilter)
-		// 	_ = w.WriteByte('>')
-		// } else {
 		_, _ = w.WriteString("```")
 		// TODO: something like 		r.writeLines(w, source, n)
 		// }
@@ -199,7 +148,7 @@ func (r *Renderer) renderCodeBlock(w util.BufWriter, source []byte, n ast.Node, 
 }
 
 func (r *Renderer) renderFencedCodeBlock(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	n := node.(*ast.FencedCodeBlock)
+	n, _ := node.(*ast.FencedCodeBlock)
 	if entering {
 		_, _ = w.WriteString("```")
 		language := n.Language(source)
@@ -215,65 +164,30 @@ func (r *Renderer) renderFencedCodeBlock(w util.BufWriter, source []byte, node a
 }
 
 func (r *Renderer) renderHTMLBlock(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	n := node.(*ast.HTMLBlock)
+	n, _ := node.(*ast.HTMLBlock)
 	if entering {
 		l := n.Lines().Len()
 		for i := 0; i < l; i++ {
 			line := n.Lines().At(i)
-			r.Writer.SecureWrite(w, line.Value(source))
+			r.Writer.Write(w, line.Value(source))
 		}
-	} else {
-		if n.HasClosure() {
-			closure := n.ClosureLine
-			r.Writer.SecureWrite(w, closure.Value(source))
-		}
+	} else if n.HasClosure() {
+		closure := n.ClosureLine
+		r.Writer.Write(w, closure.Value(source))
 	}
 	return ast.WalkContinue, nil
 }
 
-// ListAttributeFilter defines attribute names which list elements can have.
-var ListAttributeFilter = GlobalAttributeFilter.Extend(
-	[]byte("start"),
-	[]byte("reversed"),
-	[]byte("type"),
-)
-
 func (r *Renderer) renderList(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	// n := node.(*ast.List)
-	// tag := "1."
-	// if n.IsOrdered() {
-	// 	tag = "ol"
-	// }
-
-	// if entering {
-	// 	_ = w.WriteByte('<')
-	// 	_, _ = w.WriteString(tag)
-	// 	if n.IsOrdered() && n.Start != 1 {
-	// 		fmt.Fprintf(w, " start=\"%d\"", n.Start)
-	// 	}
-	// 	if n.Attributes() != nil {
-	// 		RenderAttributes(w, n, ListAttributeFilter)
-	// 	}
-	// 	_, _ = w.WriteString(">\n")
-	// } else {
-	// 	_, _ = w.WriteString("</")
-	// 	_, _ = w.WriteString(tag)
-	// 	_, _ = w.WriteString(">\n")
-	// }
 	if !entering {
 		_ = w.WriteByte('\n')
 	}
 	return ast.WalkContinue, nil
 }
 
-// ListItemAttributeFilter defines attribute names which list item elements can have.
-var ListItemAttributeFilter = GlobalAttributeFilter.Extend(
-	[]byte("value"),
-)
-
 func (r *Renderer) renderListItem(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
 	// TODO: a more efficient way?
-	list := n.Parent().(*ast.List)
+	list, _ := n.Parent().(*ast.List)
 	ordered := list.IsOrdered()
 
 	if entering {
@@ -284,11 +198,6 @@ func (r *Renderer) renderListItem(w util.BufWriter, source []byte, n ast.Node, e
 			_ = w.WriteByte(list.Marker)
 		}
 		_ = w.WriteByte(' ')
-
-		// if n.Attributes() != nil {
-		// 	_, _ = w.WriteString("<li")
-		// 	RenderAttributes(w, n, ListItemAttributeFilter)
-		// 	_ = w.WriteByte('>')
 
 		fc := n.FirstChild()
 		if fc != nil {
@@ -303,24 +212,10 @@ func (r *Renderer) renderListItem(w util.BufWriter, source []byte, n ast.Node, e
 	return ast.WalkContinue, nil
 }
 
-// ParagraphAttributeFilter defines attribute names which paragraph elements can have.
-var ParagraphAttributeFilter = GlobalAttributeFilter
-
 func (r *Renderer) renderParagraph(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
-	// if entering {
-	// 	if n.Attributes() != nil {
-	// 		_, _ = w.WriteString("<p")
-	// 		RenderAttributes(w, n, ParagraphAttributeFilter)
-	// 		_ = w.WriteByte('>')
-	// 	} else {
-	// 		_, _ = w.WriteString("<p>")
-	// 	}
-	// } else {
-	// 	_, _ = w.WriteString("</p>\n")
-	// }
 	if !entering {
 		if n.Parent().LastChild() == n {
-			_, _ = w.WriteString("\n")
+			_ = w.WriteByte('\n')
 		} else {
 			_, _ = w.WriteString("\n\n")
 		}
@@ -332,66 +227,37 @@ func (r *Renderer) renderParagraph(w util.BufWriter, source []byte, n ast.Node, 
 func (r *Renderer) renderTextBlock(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
 	// TODO: Check if this is right
 	if !entering {
-		if _, ok := n.NextSibling().(ast.Node); ok && n.FirstChild() != nil {
+		if n.NextSibling() != nil && n.FirstChild() != nil {
 			_ = w.WriteByte('\n')
 		}
 	}
 	return ast.WalkContinue, nil
 }
 
-// ThematicAttributeFilter defines attribute names which hr elements can have.
-var ThematicAttributeFilter = GlobalAttributeFilter.Extend(
-	[]byte("align"),   // [Deprecated]
-	[]byte("color"),   // [Not Standardized]
-	[]byte("noshade"), // [Deprecated]
-	[]byte("size"),    // [Deprecated]
-	[]byte("width"),   // [Deprecated]
-)
-
 func (r *Renderer) renderThematicBreak(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
 	if !entering {
 		return ast.WalkContinue, nil
 	}
 	_, _ = w.WriteString("---\n\n")
-	// if n.Attributes() != nil {
-	// 	RenderAttributes(w, n, ThematicAttributeFilter)
-	// }
 	return ast.WalkContinue, nil
 }
 
-// LinkAttributeFilter defines attribute names which link elements can have.
-var LinkAttributeFilter = GlobalAttributeFilter.Extend(
-	[]byte("download"),
-	// []byte("href"),
-	[]byte("hreflang"),
-	[]byte("media"),
-	[]byte("ping"),
-	[]byte("referrerpolicy"),
-	[]byte("rel"),
-	[]byte("shape"),
-	[]byte("target"),
-)
-
 func (r *Renderer) renderAutoLink(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	n := node.(*ast.AutoLink)
+	n, _ := node.(*ast.AutoLink)
 	if !entering {
 		return ast.WalkContinue, nil
 	}
 
 	url := n.URL(source)
-	// label := n.Label(source)
 
-	w.WriteString("<")
+	_, _ = w.WriteString("<")
 	// TODO: Labels?
 	// w.Write(label)
-	w.Write(url)
-	w.WriteString(">")
+	_, _ = w.Write(url)
+	_, _ = w.WriteString(">")
 
 	return ast.WalkContinue, nil
 }
-
-// CodeAttributeFilter defines attribute names which code elements can have.
-var CodeAttributeFilter = GlobalAttributeFilter
 
 func (r *Renderer) renderCodeSpan(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
 	// TODO:
@@ -420,42 +286,24 @@ func (r *Renderer) renderCodeSpan(w util.BufWriter, source []byte, n ast.Node, e
 	return ast.WalkContinue, nil
 }
 
-// EmphasisAttributeFilter defines attribute names which emphasis elements can have.
-var EmphasisAttributeFilter = GlobalAttributeFilter
-
 func (r *Renderer) renderEmphasis(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	n := node.(*ast.Emphasis)
+	n, _ := node.(*ast.Emphasis)
 	tag := "*"
 	// TODO: Handle nested emphasis
 	if n.Level == 2 {
 		tag = "**"
 	}
 
-	if entering {
-		_, _ = w.WriteString(tag)
-		// if n.Attributes() != nil {
-		// 	RenderAttributes(w, n, EmphasisAttributeFilter)
-		// }
-	} else {
-		_, _ = w.WriteString(tag)
-	}
+	// Write * on entering and leaving
+	_, _ = w.WriteString(tag)
 	return ast.WalkContinue, nil
 }
 
 func (r *Renderer) renderLink(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
-	n := node.(*ast.Link)
+	n, _ := node.(*ast.Link)
 
 	if entering {
 		_, _ = w.WriteString("[")
-
-		// if n.Title != nil {
-		// 	_, _ = w.WriteString(` title="`)
-		// 	r.Writer.Write(w, n.Title)
-		// 	_ = w.WriteByte('"')
-		// }
-		// if n.Attributes() != nil {
-		// 	RenderAttributes(w, n, LinkAttributeFilter)
-		// }
 	} else {
 		_, _ = w.WriteString("]")
 		_, _ = w.WriteString("(")
@@ -465,33 +313,14 @@ func (r *Renderer) renderLink(w util.BufWriter, source []byte, node ast.Node, en
 	return ast.WalkContinue, nil
 }
 
-// ImageAttributeFilter defines attribute names which image elements can have.
-var ImageAttributeFilter = GlobalAttributeFilter.Extend(
-	[]byte("align"),
-	[]byte("border"),
-	[]byte("crossorigin"),
-	[]byte("decoding"),
-	[]byte("height"),
-	[]byte("importance"),
-	[]byte("intrinsicsize"),
-	[]byte("ismap"),
-	[]byte("loading"),
-	[]byte("referrerpolicy"),
-	[]byte("sizes"),
-	[]byte("srcset"),
-	[]byte("usemap"),
-	[]byte("width"),
-)
-
 func (r *Renderer) renderImage(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
 	if !entering {
 		return ast.WalkContinue, nil
 	}
-	n := node.(*ast.Image)
+	n, _ := node.(*ast.Image)
 
 	_, _ = w.WriteString("![")
-	// TODO: Alt
-	// _, _ = w.Write(nodeToHTMLText(n, source))
+	_, _ = w.Write(n.Text(source))
 	_, _ = w.WriteString("](")
 	_, _ = w.Write(n.Destination)
 
@@ -501,9 +330,6 @@ func (r *Renderer) renderImage(w util.BufWriter, source []byte, node ast.Node, e
 		r.Writer.Write(w, n.Title)
 	}
 	_ = w.WriteByte(')')
-	// if n.Attributes() != nil {
-	// 	RenderAttributes(w, n, ImageAttributeFilter)
-	// }
 	return ast.WalkSkipChildren, nil
 }
 
@@ -512,7 +338,7 @@ func (r *Renderer) renderRawHTML(w util.BufWriter, source []byte, node ast.Node,
 		return ast.WalkSkipChildren, nil
 	}
 
-	n := node.(*ast.RawHTML)
+	n, _ := node.(*ast.RawHTML)
 	l := n.Segments.Len()
 	for i := 0; i < l; i++ {
 		segment := n.Segments.At(i)
@@ -526,15 +352,15 @@ func (r *Renderer) renderText(w util.BufWriter, source []byte, node ast.Node, en
 	if !entering {
 		return ast.WalkContinue, nil
 	}
-	n := node.(*ast.Text)
+	n, _ := node.(*ast.Text)
 	segment := n.Segment
 	if n.IsRaw() {
-		r.Writer.RawWrite(w, segment.Value(source))
+		r.Writer.Write(w, segment.Value(source))
 	} else {
 		value := segment.Value(source)
 		r.Writer.Write(w, value)
 		if n.HardLineBreak() || n.SoftLineBreak() {
-			w.WriteByte('\n')
+			_ = w.WriteByte('\n')
 		}
 	}
 	return ast.WalkContinue, nil
@@ -544,258 +370,34 @@ func (r *Renderer) renderString(w util.BufWriter, source []byte, node ast.Node, 
 	if !entering {
 		return ast.WalkContinue, nil
 	}
-	n := node.(*ast.String)
+	n, _ := node.(*ast.String)
+	// TODO: what's the difference?
 	if n.IsCode() {
 		_, _ = w.Write(n.Value)
 	} else {
-		if n.IsRaw() {
-			r.Writer.RawWrite(w, n.Value)
-		} else {
-			r.Writer.Write(w, n.Value)
-		}
+		r.Writer.Write(w, n.Value)
 	}
 	return ast.WalkContinue, nil
 }
-
-var dataPrefix = []byte("data-")
-
-// // RenderAttributes renders given node's attributes.
-// // You can specify attribute names to render by the filter.
-// // If filter is nil, RenderAttributes renders all attributes.
-// func RenderAttributes(w util.BufWriter, node ast.Node, filter util.BytesFilter) {
-// 	for _, attr := range node.Attributes() {
-// 		if filter != nil && !filter.Contains(attr.Name) {
-// 			if !bytes.HasPrefix(attr.Name, dataPrefix) {
-// 				continue
-// 			}
-// 		}
-// 		_, _ = w.WriteString(" ")
-// 		_, _ = w.Write(attr.Name)
-// 		_, _ = w.WriteString(`="`)
-// 		// TODO: convert numeric values to strings
-// 		_, _ = w.Write(util.EscapeHTML(attr.Value.([]byte)))
-// 		_ = w.WriteByte('"')
-// 	}
-// }
 
 // A Writer interface writes textual contents to a writer.
 type Writer interface {
 	// Write writes the given source to writer with resolving references and unescaping
 	// backslash escaped characters.
 	Write(writer util.BufWriter, source []byte)
-
-	// RawWrite writes the given source to writer without resolving references and
-	// unescaping backslash escaped characters.
-	RawWrite(writer util.BufWriter, source []byte)
-
-	// TODO: Needed?
-	// SecureWrite writes the given source to writer with replacing insecure characters.
-	SecureWrite(writer util.BufWriter, source []byte)
 }
 
-// var replacementCharacter = []byte("\ufffd")
-
-// // A WriterConfig struct has configurations for the HTML based writers.
-// type WriterConfig struct {
-// 	// EscapedSpace is an option that indicates that a '\' escaped half-space(0x20) should not be rendered.
-// 	EscapedSpace bool
-// }
-
-// // A WriterOption interface sets options for HTML based writers.
-// type WriterOption func(*WriterConfig)
-
-// // WithEscapedSpace is a WriterOption indicates that a '\' escaped half-space(0x20) should not be rendered.
-// func WithEscapedSpace() WriterOption {
-// 	return func(c *WriterConfig) {
-// 		c.EscapedSpace = true
-// 	}
-// }
-
-type defaultWriter struct {
-	// WriterConfig
-}
+type defaultWriter struct{}
 
 // NewWriter returns a new Writer.
-func NewWriter( /*opts ...WriterOption*/ ) Writer {
+func NewWriter() Writer {
 	w := &defaultWriter{}
-	// for _, opt := range opts {
-	// 	opt(&w.WriterConfig)
-	// }
 	return w
 }
 
-func escapeRune(writer util.BufWriter, r rune) {
-	if r < 256 {
-		v := util.EscapeHTMLByte(byte(r))
-		if v != nil {
-			_, _ = writer.Write(v)
-			return
-		}
-	}
-	_, _ = writer.WriteRune(util.ToValidRune(r))
-}
-
-func (d *defaultWriter) SecureWrite(writer util.BufWriter, source []byte) {
-	// n := 0
-	// l := len(source)
-	// for i := 0; i < l; i++ {
-	// 	if source[i] == '\u0000' {
-	// 		_, _ = writer.Write(source[i-n : i])
-	// 		n = 0
-	// 		_, _ = writer.Write(replacementCharacter)
-	// 		continue
-	// 	}
-	// 	n++
-	// }
-	// if n != 0 {
-	_, _ = writer.Write(source)
-	// }
-}
-
-func (d *defaultWriter) RawWrite(writer util.BufWriter, source []byte) {
-	_, _ = writer.Write(source)
-	// n := 0
-	// l := len(source)
-	// for i := 0; i < l; i++ {
-	// 	v := util.EscapeHTMLByte(source[i])
-	// 	if v != nil {
-	// 		_, _ = writer.Write(source[i-n : i])
-	// 		n = 0
-	// 		_, _ = writer.Write(v)
-	// 		continue
-	// 	}
-	// 	n++
-	// }
-	// if n != 0 {
-	// 	_, _ = writer.Write(source[l-n:])
-	// }
-}
-
 func (d *defaultWriter) Write(writer util.BufWriter, source []byte) {
-	// escaped := false
-	// var ok bool
-	// limit := len(source)
-	// n := 0
-	// for i := 0; i < limit; i++ {
-	// 	c := source[i]
-	// 	if escaped {
-	// 		if util.IsPunct(c) {
-	// 			d.RawWrite(writer, source[n:i-1])
-	// 			n = i
-	// 			escaped = false
-	// 			continue
-	// 		}
-	// 		if d.EscapedSpace && c == ' ' {
-	// 			d.RawWrite(writer, source[n:i-1])
-	// 			n = i + 1
-	// 			escaped = false
-	// 			continue
-	// 		}
-	// 	}
-	// 	if c == '\x00' {
-	// 		d.RawWrite(writer, source[n:i])
-	// 		d.RawWrite(writer, replacementCharacter)
-	// 		n = i + 1
-	// 		escaped = false
-	// 		continue
-	// 	}
-	// 	if c == '&' {
-	// 		pos := i
-	// 		next := i + 1
-	// 		if next < limit && source[next] == '#' {
-	// 			nnext := next + 1
-	// 			if nnext < limit {
-	// 				nc := source[nnext]
-	// 				// code point like #x22;
-	// 				if nnext < limit && nc == 'x' || nc == 'X' {
-	// 					start := nnext + 1
-	// 					i, ok = util.ReadWhile(source, [2]int{start, limit}, util.IsHexDecimal)
-	// 					if ok && i < limit && source[i] == ';' && i-start < 7 {
-	// 						v, _ := strconv.ParseUint(util.BytesToReadOnlyString(source[start:i]), 16, 32)
-	// 						d.RawWrite(writer, source[n:pos])
-	// 						n = i + 1
-	// 						escapeRune(writer, rune(v))
-	// 						continue
-	// 					}
-	// 					// code point like #1234;
-	// 				} else if nc >= '0' && nc <= '9' {
-	// 					start := nnext
-	// 					i, ok = util.ReadWhile(source, [2]int{start, limit}, util.IsNumeric)
-	// 					if ok && i < limit && i-start < 8 && source[i] == ';' {
-	// 						v, _ := strconv.ParseUint(util.BytesToReadOnlyString(source[start:i]), 10, 32)
-	// 						d.RawWrite(writer, source[n:pos])
-	// 						n = i + 1
-	// 						escapeRune(writer, rune(v))
-	// 						continue
-	// 					}
-	// 				}
-	// 			}
-	// 		} else {
-	// 			start := next
-	// 			i, ok = util.ReadWhile(source, [2]int{start, limit}, util.IsAlphaNumeric)
-	// 			// entity reference
-	// 			if ok && i < limit && source[i] == ';' {
-	// 				name := util.BytesToReadOnlyString(source[start:i])
-	// 				entity, ok := util.LookUpHTML5EntityByName(name)
-	// 				if ok {
-	// 					d.RawWrite(writer, source[n:pos])
-	// 					n = i + 1
-	// 					d.RawWrite(writer, entity.Characters)
-	// 					continue
-	// 				}
-	// 			}
-	// 		}
-	// 		i = next - 1
-	// 	}
-	// 	if c == '\\' {
-	// 		escaped = true
-	// 		continue
-	// 	}
-	// 	escaped = false
-	// }
-	d.RawWrite(writer, source)
+	_, _ = writer.Write(source)
 }
 
 // DefaultWriter is a default instance of the Writer.
 var DefaultWriter = NewWriter()
-
-var bDataImage = []byte("data:image/")
-var bPng = []byte("png;")
-var bGif = []byte("gif;")
-var bJpeg = []byte("jpeg;")
-var bWebp = []byte("webp;")
-var bSvg = []byte("svg+xml;")
-var bJs = []byte("javascript:")
-var bVb = []byte("vbscript:")
-var bFile = []byte("file:")
-var bData = []byte("data:")
-
-// IsDangerousURL returns true if the given url seems a potentially dangerous url,
-// otherwise false.
-func IsDangerousURL(url []byte) bool {
-	if bytes.HasPrefix(url, bDataImage) && len(url) >= 11 {
-		v := url[11:]
-		if bytes.HasPrefix(v, bPng) || bytes.HasPrefix(v, bGif) ||
-			bytes.HasPrefix(v, bJpeg) || bytes.HasPrefix(v, bWebp) ||
-			bytes.HasPrefix(v, bSvg) {
-			return false
-		}
-		return true
-	}
-	return bytes.HasPrefix(url, bJs) || bytes.HasPrefix(url, bVb) ||
-		bytes.HasPrefix(url, bFile) || bytes.HasPrefix(url, bData)
-}
-
-func nodeToHTMLText(n ast.Node, source []byte) []byte {
-	var buf bytes.Buffer
-	for c := n.FirstChild(); c != nil; c = c.NextSibling() {
-		if s, ok := c.(*ast.String); ok && s.IsCode() {
-			buf.Write(s.Text(source))
-		} else if !c.HasChildren() {
-			buf.Write(util.EscapeHTML(c.Text(source)))
-		} else {
-			buf.Write(nodeToHTMLText(c, source))
-		}
-	}
-	return buf.Bytes()
-}
